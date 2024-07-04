4e4dc967a291cd35fbc602146e84fd10422d7a58
mayankkuhaad <mayankkuhaad@gmail.com>
2024-07-04 15:22:10 +0530

    sumZero

---
+++ sumZero1304.js
@@ -0,0 +1,12 @@
+var sumZero = function(n) {
+    let arr = [];
+    for(let i=1; i<=Math.floor(n/2); i++){
+        arr.push(i);
+        arr.push(-i);
+    }
+    if(n % 2 !== 0){
+        arr.push(0);
+    }
+    return arr
+};
+console.log(sumZero(5))
