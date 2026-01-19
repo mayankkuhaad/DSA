fccdeabebc8b80659a79d97beb262c90ef685e18
Mayank Kuhaad <mayankkuhaad@gmail.com>
2026-01-19 08:56:58 +0530

    33!

--- arrays/twoSum1.js
+++ arrays/twoSum1.js
@@ -11,20 +11,3 @@ function twoSum(nums, target){
 }
 
 console.log(twoSum([2,7,11,15], 9));
-
-// Input: nums = [2, 7, 11, 15], target = 9
-// Output: [0, 1]
-
-/*
-DRY RUN
-
-nums[0] = 2;
-c =  9 - 2  = 7
-map.has(7) => NO
-so, set in map, {2, 0}
- 
-num[1] = 7;
-c = 9 - 7 = 2;
-is 2 in map? yes.
-so, return [2,0];
-*/
